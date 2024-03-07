75d419d0f3856d777b6409cfcf34fc727cb11a4d
maria-ioana-hurmuzache <161624515+maria-ioana-hurmuzache@users.noreply.github.com>
2024-03-07 10:24:55 +0200

    Update GlobalFunctions.cpp

--- lab2/pb2/GlobalFunctions.cpp
+++ lab2/pb2/GlobalFunctions.cpp
@@ -3,7 +3,7 @@
 //am considerat ca numele se compara tinand cont de ordinea lor alfabetica, caz in care valoarea "mai mare" este de fapt numele care este alfabetic mai jos pe lista
 short int compareName(Student& s1, Student& s2)
 {
-	if ((strcmp(s1.GetName(), s2.GetName())) > 0)
+	if ((strcmp(s1.GetName(), s2.GetName())) < 0)
 		return -1;
 	if ((strcmp(s1.GetName(), s2.GetName())) == 0)
 		return 0;
